93d2dec71f35a442bceacde40210fd7764dab2d5
Cathy Stein <3420442+skyesong@users.noreply.github.com>
2020-02-27 22:42:55 -0500

    color scheme, start scaffolding

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="Home">
+      <header className="Home-band">
+        <h1>Catherine Stein</h1>
+        <p>Web Developer, Musician</p>
       </header>
-    </div>
+      <section>
+        <h2>Bio</h2>
+        <p>lorem ipsum dolor sit amet</p>
+      </section>
+      <section className="Home-band">
+        <h2>Links to stuff</h2>
+        <ul>
+          <li>lorem</li>
+          <li>ipsum</li>
+          <li>dolor</li>
+        </ul>
+      </section>
+      
+    </main>
   );
 }
 
